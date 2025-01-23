a75208b1094855144372b97c60d4aaf5f6911bde
Lucas <lucaswsb52@gmail.com>
2025-01-23 18:17:38 -0300

    asdasdas

--- src/screens/DesafiosListScreen.js
+++ src/screens/DesafiosListScreen.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function DesafiosListScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Bem-vindo à tela de Desafios!</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
@@ -20,5 +29,9 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     color: '#333',
+  },
+  map: {
+    width: '100%',
+    height: '80%',
   },
 });
