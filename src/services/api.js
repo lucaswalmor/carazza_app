4896517a50c4e0559ed55df099f4bd4555928fac
Lucas <lucaswsb52@gmail.com>
2025-02-02 00:25:59 -0300

    asdsadasd

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://c7aa-2804-1e68-c209-dc0a-1819-33b6-f9d9-a3fa.ngrok-free.app/api',
-    // baseURL: 'https://carazza.lksoftware.com.br/public/api',
+    // baseURL: 'https://c7aa-2804-1e68-c209-dc0a-1819-33b6-f9d9-a3fa.ngrok-free.app/api',
+    baseURL: 'https://carazza.lksoftware.com.br/public/api',
     timeout: 0,
     headers: {
         'Content-Type': 'application/json',
